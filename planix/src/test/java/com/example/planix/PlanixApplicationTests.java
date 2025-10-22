a8f03f48add831d6ec1c31034d8427bb220aa062
Ivett <tarivett950@gmail.com>
2025-10-22 16:13:44 +0300

    Initial commit

---
+++ planix/src/test/java/com/example/planix/PlanixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
